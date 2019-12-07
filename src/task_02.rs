8e2b58749265507b4c3510c2e64936356357c8b2
Ales Bolka <oxudes@gmail.com>
2019-12-07 12:28:34 +0100

    Restructured project files

--- src/task_02.rs
+++ src/task_02.rs
@@ -1,8 +1,4 @@
-fn main() {
-  task2();
-}
-
-fn runIntCode(mut input: Vec<u32>) -> u32 {
+pub fn run_int_code(mut input: Vec<u32>) -> u32 {
   let mut ii = 0;
   loop {
       if ii >= input.len() {
@@ -29,18 +25,20 @@ fn runIntCode(mut input: Vec<u32>) -> u32 {
   }
 }
 
-fn task2() {
+pub const DEMO: u32 = 9507;
+
+pub fn demo() -> u32 {
   let input: Vec<u32> = vec![1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,1,19,5,23,2,13,23,27,1,10,27,31,2,6,31,35,1,9,35,39,2,10,39,43,1,43,9,47,1,47,9,51,2,10,51,55,1,55,9,59,1,59,5,63,1,63,6,67,2,6,67,71,2,10,71,75,1,75,5,79,1,9,79,83,2,83,10,87,1,87,6,91,1,13,91,95,2,10,95,99,1,99,6,103,2,13,103,107,1,107,2,111,1,111,9,0,99,2,14,0,0];
   let expected: u32 = 19690720;
   let mut result: u32 = 0;
 
 
-  'outer: for ii in (0..99) {
-      for jj in (0..99) {
+  'outer: for ii in 0..(input.len() as u32 - 1) {
+      for jj in 0..(input.len() as u32 - 1) {
           let mut clone = input.clone();
           clone[1] = ii;
           clone[2] = jj;
-          let out = runIntCode(clone);
+          let out = run_int_code(clone);
           if out == expected {
               result = 100 * ii + jj;
               break 'outer;
@@ -48,5 +46,5 @@ fn task2() {
       }
   };
 
-  println!("{}", result);
+  result
 }
