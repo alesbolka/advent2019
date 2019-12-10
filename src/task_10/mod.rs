0520d2122d317f644ff508f5170b3cb7ca7a9b88
Ales Bolka <oxudes@gmail.com>
2019-12-10 23:05:05 +0100

    Giving up for the night

--- src/task_10/mod.rs
+++ src/task_10/mod.rs
@@ -1,6 +1,6 @@
 mod input;
 use std::f32::{ INFINITY, NEG_INFINITY };
-const EPS: f32 = 0.000000001;
+const EPS: f32 = 0.0000000001;
 
 struct Map {
     asteroids: Vec<Asteroid>,
@@ -8,13 +8,26 @@ struct Map {
 }
 
 impl Map {
-    fn get_seen_asteroids(&self, index: usize) -> u32 {
+    pub fn get_seen_asteroids(&self, index: usize) -> u32 {
         let mut count: u32 = 0; // Maximum
         for line in &self.lines {
+            // println!("Line: {:?}\n {:?} => {}", line, line.roids, line.seen_roids(index));
             count += line.seen_roids(index);
         }
 
         count
+    }
+
+    pub fn get_highest_visible(&self) -> u32 {
+        let mut max = 0;
+        for ii in 0..self.asteroids.len() {
+            let res = self.get_seen_asteroids(ii);
+            if res > max {
+                max = res;
+            }
+        }
+
+        max
     }
 }
 
@@ -50,7 +63,7 @@ impl Line {
         Line{
             k,
             n,
-            min: 0.0,
+            min: INFINITY,
             min_index: 0,
             max: 0.0,
             max_index: 0,
@@ -88,7 +101,7 @@ impl Line {
             }
             if self.min > y {
                 self.min = y;
-                self.max_index = ii;
+                self.min_index = ii;
             }
             return;
         }
@@ -98,7 +111,7 @@ impl Line {
         }
         if self.min > x {
             self.min = x;
-            self.max_index = ii;
+            self.min_index = ii;
         }
     }
 
@@ -143,15 +156,16 @@ fn parse_map(raw: &str) -> Map {
                 if ii == counter {
                     continue;
                 }
+
                 let mut line = Line::from_roids(&asteroids[counter], roid);
                 for sub in &mut lines {
                     if &line == sub {
                         sub.add_point(xx, yy, counter);
-                        break 'roidLoop;
+                        continue 'roidLoop;
                     }
                 }
-                line.add_point(xx, yy, counter);
                 line.add_point(roid.x, roid.y, ii);
+                line.add_point(xx, yy, counter);
 
                 lines.push(line);
             }
@@ -225,14 +239,74 @@ mod tests {
     }
 
     #[test]
-    fn demo_1_ales() {
+    fn simple_1() {
+        // #.#
+        // ...
+        // #.#
+        let map = parse_map("#.#\n...\n#.#");
+        assert_eq!(map.lines.len(), 6);
+        assert_eq!(3, map.get_seen_asteroids(0));
+        assert_eq!(3, map.get_seen_asteroids(1));
+        assert_eq!(3, map.get_seen_asteroids(2));
+        assert_eq!(3, map.get_seen_asteroids(3));
+    }
+
+    #[test]
+    fn simple_2() {
+        // #.#.#
+        // ..#..
+        // #.#.#
+        let map = parse_map("#.#.#\n..#..\n#.#.#");
+        assert_eq!(map.lines.len(), 7);
+        assert_eq!(4, map.get_seen_asteroids(0));
+        assert_eq!(6, map.get_seen_asteroids(3));
+    }
+
+    #[test]
+    fn simple_3() {
+        // #####
+        // ..#..
+        // #.#.#
+        let map = parse_map("#####\n..#..\n#.#.#");
+        assert_eq!(6, map.get_seen_asteroids(1));
+        // assert_eq!(6, map.get_seen_asteroids(3));
+    }
+
+    #[test]
+    fn demo_1() {
         // .7..7
         // .....
         // 67775
         // ....7
         // ...87
-        let map = parse_map("#.#\n...\n#.#");
-        assert_eq!(map.lines.len(), 6);
-        assert_eq!(3, map.get_seen_asteroids(0));
+        let map = parse_map(input::DEMO1);
+        for (ii, &expected) in vec![7,7,6,7,7,7,5,7,8,7].iter().enumerate() {
+            assert_eq!(expected, map.get_seen_asteroids(ii), "Failed comparison {}", ii);
+        }
+        assert_eq!(8, map.get_highest_visible());
+    }
+
+    #[test]
+    fn demo_2() {
+        let map = parse_map(input::DEMO2);
+        assert_eq!(33, map.get_highest_visible());
+    }
+
+    #[test]
+    fn demo_3() {
+        let map = parse_map(input::DEMO3);
+        assert_eq!(35, map.get_highest_visible());
+    }
+
+    #[test]
+    fn demo_4() {
+        let map = parse_map(input::DEMO4);
+        assert_eq!(41, map.get_highest_visible());
+    }
+
+    #[test]
+    fn demo_5_ales() {
+        let map = parse_map(input::DEMO5);
+        assert_eq!(210, map.get_highest_visible());
     }
 }
